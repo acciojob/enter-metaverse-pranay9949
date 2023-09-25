866447da3095799cb6ca76adf04cb492db030e03
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-25 16:17:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let b=document.getElementById("enterBtn");
 function change(){
 	let c=document.createElement("h1");
 	c.innerText="Entered Metaverse";
-	a.replaceWith(c);
+		a.replaceWith(c);
 	
 }
 b.addEventListener('click',change);
