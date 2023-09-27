5b2cbf55f467d71d3878bf76e5d821104df7e99e
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-27 17:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
 let a=document.getElementById("status");
-let b=document.getElementById("enterBtn");
+
 function change(){
 	let c=document.createElement("h1");
 	c.innerText="Entered Metaverse";
-		a.replaceWith(c);
+	document.body.insertBefore(c,a);
+	a.remove();
 	
 }
 b.addEventListener('click',change);
